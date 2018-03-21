48d7789c6b756c192c120ad4da16b18f6f90ff00
CertainlyT <xcm1321@gmail.com>
2018-03-21 20:49:04 +0900

    lab1

---
+++ lab1/public/lab1/lab1.js
@@ -0,0 +1,3 @@
+console.log("lab1.js.console.log() -- Hello World! 나는 정지현이다.");
+document.write("<h1> lab1.js.document.write() -- 하하하, 여기도 정지현!!! </h1>");
+alert("lab1.js.alert() -- 소프트웨어학부(20171701)");
